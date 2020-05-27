3bf7f2565cd0387b801fae4efbea676451b32e8b
Natalia <Natalia@Monica>
2020-05-27 16:32:33 -0300

    Asunto:Inicialización del proyecto. Responsable:Monica Natalia Tapia. TPN°5:Inicialización del proyecto.

---
+++ src/test/java/ar/edu/unju/fi/Tapia3516Tpn5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tapia3516Tpn5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
